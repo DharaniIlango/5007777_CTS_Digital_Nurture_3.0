1c7e514242208abbb726e84e6c30d59c8ba73af9
Dharani I <dharaniilango1209@gmail.com>
2024-07-30 19:37:25 +0530

    directory structure updation

---
+++ Week_01/Design_Patterns_and_Principles/FactoryMethodPattern/app/src/main/java/org/digital/nurture/MainTest.java
@@ -0,0 +1,23 @@
+package org.digital.nurture;
+
+import org.digital.nurture.document.Document;
+import org.digital.nurture.factory.DocumentFactory;
+import org.digital.nurture.factory.WordDocumentFactory;
+import org.digital.nurture.factory.PdfDocumentFactory;
+import org.digital.nurture.factory.ExcelDocumentFactory;
+
+public class MainTest {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
