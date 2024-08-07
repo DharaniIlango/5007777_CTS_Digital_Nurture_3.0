0e485817695180f3579d9dd6b7b06a8a8582a146
Dharani I <dharaniilango1209@gmail.com>
2024-08-07 20:06:18 +0530

    Exercise 06 - Completed

--- Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/Main.java
+++ Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/Main.java
@@ -1,25 +1,25 @@
 package com.library;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.library.service.BookService;
 
 public class Main {
   public static void main(String[] args) {
     // Load Spring context from XML configuration file
-    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    ApplicationContext context = new AnnotationConfigApplicationContext("com.library");
 
     // Retrieve BookService bean and test it
-    BookService bookService = (BookService) context.getBean("bookService");
-    
-    // Test the bean
+    BookService bookService = context.getBean(BookService.class);
+
+    // Test the bean (for example, you might want to call some method here)
     System.out.println("BookService bean loaded: " + bookService);
   }
 }
 
 //Output:
 /*
-    --- exec:3.1.0:exec (default-cli) @ librarymanagement ---
-BookService bean loaded: com.library.service.BookService@10e41621
+--- exec:3.1.0:exec (default-cli) @ librarymanagement ---
+BookService bean loaded: com.library.service.BookService@4a22f9e2
  */
--- Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/service/BookService.java
+++ Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/service/BookService.java
@@ -1,11 +1,15 @@
 package com.library.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.library.repository.BookRepository;
 
+@Service
 public class BookService {
   private BookRepository bookRepository;
 
-  // Setter for BookRepository
+  @Autowired
   public void setBookRepository(BookRepository bookRepository) {
     this.bookRepository = bookRepository;
   }
