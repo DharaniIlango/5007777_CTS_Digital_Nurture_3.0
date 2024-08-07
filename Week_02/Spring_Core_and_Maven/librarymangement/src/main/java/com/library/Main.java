1b4b13e09fca489097d87bc9c25f865e481490b3
Dharani I <dharaniilango1209@gmail.com>
2024-08-07 19:39:53 +0530

    Exercise 03 - Completed

--- Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/Main.java
+++ Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/Main.java
@@ -10,13 +10,16 @@ public class Main {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         BookService bookService = (BookService) context.getBean("bookService");
 
-        // Test the configuration
+        // Test the configuration and logging
         System.out.println("BookService bean: " + bookService);
-        bookService.performService(); // Invoke a method to see if dependency is injected
+        bookService.performService(); // Invoke a method to see logging
     }
 }
 
 //Output:
-/* --- exec:3.1.0:exec (default-cli) @ librarymanagement ---
-   BookService bean: com.library.service.BookService@670002
-   Service is performing using: com.library.repository.BookRepository@1f0f1111 */
+/*
+ --- exec:3.1.0:exec (default-cli) @ librarymanagement ---
+BookService bean: com.library.service.BookService@11d8ae8b
+Service is performing using: com.library.repository.BookRepository@404bbcbd
+[main] INFO com.library.aspect.LoggingAspect - void com.library.service.BookService.performService() executed in 16ms
+*/
---
+++ Week_02/Spring_Core_and_Maven/librarymangement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* com.library.service..*(..))")
+    public void serviceMethods() {
+        // Pointcut for methods in service package
+    }
+
+    @Around("serviceMethods()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        logger.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
